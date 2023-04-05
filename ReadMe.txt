fb338cdb2a00bc93ba68fe5b1c2e0c9ab0ef0821
shahat <me6847570@gmail.com>
2023-04-05 22:14:18 +0200

    post , get , delete , filter , fort data using json server

---
+++ ReadMe.txt
@@ -0,0 +1,18 @@
+=============================
+ =>  1 : install json server 
+=============================
+
+   npm i -g json-server 
+
+
+=============================
+ =>  2 : make db.json file  and put it inside data folder 
+=============================
+
+
+=============================
+ =>  3 : watching the json by creating api end point   
+=============================
+
+    json-server --watch data/bd.json
+
--- js/details.js
+++ js/details.js
@@ -1,1 +1,35 @@
 // javascript for details.html
+const id = new URLSearchParams(window.location.search).get("id");
+
+const container = document.querySelector(".details");
+const deleteBtn = document.querySelector(".delete");
+
+/* render details function for the spesefic id 
+  1 - once we click  send the request using async js fetch function with the id 
+*/
+const renderDetails = async () => {
+  const res = await fetch("http://localhost:3000/posts/" + id);
+  const post = await res.json();
+
+  const template = ` 
+  <h1>${post.title}</h1>
+  <p>${post.body}</p>             
+  `;
+  container.innerHTML = template;
+};
+
+/* handel the delete blog  and doning the delete event */
+
+const deleteBlog = () => {
+  deleteBtn.addEventListener("click", async (e) => {
+    console.log(e);
+    const res = await fetch("http://localhost:3000/posts/" + id, {
+      method: "DELETE",
+    });
+    window.location.replace("/index.html");
+  });
+};
+
+deleteBlog();
+
+window.addEventListener("DOMContentLoaded", () => renderDetails());
